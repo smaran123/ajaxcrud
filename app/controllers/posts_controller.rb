c5d87085344c3947ad458c5d052dec1557679e8b
bhagathreddy123 <katta_reddy04@yahoo.com>
2016-02-24 22:42:37 +0530

    ajaxify post resource

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,68 +6,58 @@ class PostsController < ApplicationController
       format.js
     end
   end
-  
+
   def index
     @posts = Post.all
   end
 
- 
+
   def show
   end
 
- 
+
   def new
     @post = Post.new
+    respond_to do |format|
+      format.js
+    end
   end
 
 
   def edit
+    respond_to do |format|
+      format.js
+    end
   end
 
 
   def create
-    @post = Post.new(post_params)
-
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
-        format.json { render :show, status: :created, location: @post }
-      else
-        format.html { render :new }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
-    end
+     @posts = Post.all
+     @post = Post.create(post_params)
   end
 
 
   def update
-    respond_to do |format|
-      if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-        format.json { render :show, status: :ok, location: @post }
-      else
-        format.html { render :edit }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
-    end
+    @posts = Post.all
+     @post.update(post_params)
+  
   end
 
   def destroy
     @post.destroy
     respond_to do |format|
-      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
-      format.json { head :no_content }
+      format.js
     end
   end
 
   private
 
-    def set_post
-      @post = Post.find(params[:id])
-    end
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
-    def post_params
-      params.require(:post).permit(:name, :description)
-    end
+  def post_params
+    params.require(:post).permit(:name, :description)
+  end
 
 end
